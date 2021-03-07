be42f82f92066baaff3d55abc0f7d89a836d5f74
Gerardo Lareu <lareugerardo@gmail.com>
2021-03-07 18:24:41 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/junior/challenge_1';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/junior/challenge_1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__title">Titulo</div>
+      <div className="header__desciption--red">Descipcion roja</div>
+      <div className="header__desciption--azul">Descipcion azul</div>
+    </div>
+  )
+}
+
+export default Header;
